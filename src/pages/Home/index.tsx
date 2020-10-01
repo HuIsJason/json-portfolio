a77b6d611677d63325a7c0999d03e8e8c89a644a
Jason Hu <jason.h.7249@gmail.com>
2020-10-01 01:29:35 -0400

    added repo link for portfolio project

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
 
 const Home: React.FC = () => {
   return (
     <>
       Under construction... for now you can check out the Resume and Projects
       pages!
-      <Grid container>
-        <Grid item>Hi</Grid>
-        <Grid item>2nd</Grid>
-      </Grid>
     </>
   );
 };
