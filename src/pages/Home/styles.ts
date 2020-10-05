dc301982b8647fc0361722cea70386b828b595a6
Jason Hu <jason.h.7249@gmail.com>
2020-10-05 18:09:17 -0400

    completed Home page

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,13 @@
+import { makeStyles, Theme } from '@material-ui/core';
+
+export default makeStyles((theme: Theme) => ({
+  pic: {
+    width: '300px',
+    height: '300px',
+    borderRadius: '50%',
+    marginTop: '25px',
+    marginLeft: '19%',
+    marginBottom: '26px',
+    display: 'block',
+  },
+}));
